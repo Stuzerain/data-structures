d8b503ace0699a0de3767e99462012ade34efbdf
Stu Hanford <shanford3@gmail.com>
2020-08-20 16:00:57 -0700

    Implement linkedList and add notes on complexity

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,51 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    var currentNode = new Node(value);
+    if (list.head === null) {
+      list.head = currentNode;
+      list.tail = currentNode;
+    } else {
+      list.tail.next = currentNode;
+    }
+    list.tail = currentNode;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    var removed = list.head.value;
+    var newHead = list.head.next;
+    list.head = list.head.next;
+    // delete list.head;
+
+    return removed;
   };
 
-  list.contains = function(target) {
+
+  // I: node
+  // O: boolean
+  list.contains = function (target) {
+    // check value of value property of each object
+    // recurse on next property
+
+    var checkForVal = function (node) {
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return checkForVal(node.next);
+    };
+    return checkForVal(list.head);
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
@@ -24,6 +54,23 @@ var Node = function(value) {
   return node;
 };
 
+// What is the time complexity of the above functions?
+//   LinkedList: should be constant
+//   addToTail: should be constant
+//   removeFromHead: should be constant
+//   contains: should be linear
+//   Node: should be constant
+
 /*
- * Complexity: What is the time complexity of the above functions?
+
+{value: 5, next: {...}}
+
+  {a: {b: {c: {d: }}}}
+
+  removed = a: asdf   line 18
+  newHead = {b: {c: {d: }}}   line 19
+  head = newHead   line 20
+
+  where does the original head ({a: ...}) go? We aren't explicitly deleting it, but we aren't referencing it, so what happens?
+
  */
