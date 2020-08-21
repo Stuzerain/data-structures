07e5e58eb22e512012ac35fa2f0de885046add88
Stu Hanford <shanford3@gmail.com>
2020-08-21 11:33:57 -0700

    Implement set using prototypal instantiation pattern

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  if (this._storage[item] !== undefined) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   ANSWER: All are constant.
  */
