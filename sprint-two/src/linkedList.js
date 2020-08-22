10d05f4708e768c79ffe8c359cabf9ac4de9ef60
Stu Hanford <shanford3@gmail.com>
2020-08-22 15:23:40 -0700

    Implement new bookmark method and test for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -39,7 +39,21 @@ var LinkedList = function () {
       }
       return checkForVal(node.next);
     };
+
     return checkForVal(list.head);
+  };
+
+  // New method that returns a copy of target node without methods, but including the proper links to every next value. Current use case is for viewing a shorter version of a long linked list from only past a certain value. Additional use cases would depend on the ability to modify specific nodes and usage of this in list methods in a linked list, which are not implemented here. Implementation of methods is limited by the requirement to use functional instantiation pattern for linkedList--given a pseudoclassical instantiation pattern, the keyword 'this' could be used in the methods, and linkedList's methods could be properly copied to the returned bookmark.
+  list.bookmark = function (target) {
+
+    var findVal = function (node) {
+      if (node.value === target) {
+        return node;
+      }
+      return findVal(node.next);
+    };
+
+    return findVal(list.head);
   };
 
   return list;
